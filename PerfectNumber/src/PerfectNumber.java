7082522192545eaa5616159ba1b0abc075d96a11
Akhil Nayabu <akhildn@gmail.com>
2017-11-26 12:42:43 -0500

    -Fixed logic
    -time to taken to concurrent execution was different before.

--- PerfectNumber/src/PerfectNumber.java
+++ PerfectNumber/src/PerfectNumber.java
@@ -1,21 +1,30 @@
 public class PerfectNumber {
     public static  void main(String args[]){
-        long startTime = System.nanoTime();
+        long startTime = System.currentTimeMillis();
         System.out.println("6 is perfect? " + isPerfect(6));
-        System.out.println("33550336 is perfect : " + isPerfect(33550336));
-        System.out.println("33550337 is perfect : " + isPerfect(33550337));
-        long endTime = System.nanoTime();
+        System.out.println("533550336 is perfect : " + isPerfect(533550336));
+        System.out.println("633550337 is perfect : " + isPerfect(633550337));
+        System.out.println("935503371 is perfect : " + isPerfect(935503371));
+        System.out.println("735503371 is perfect : " + isPerfect(735503371));
+        long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
-        System.out.println("Duration :" + duration);
+        System.out.println("Duration for Sequential :" + duration);
 
         Thread t1= new PerfectThread(6);
-        Thread t2= new PerfectThread(33550336);
-        Thread t3= new PerfectThread(33550337);
-        long startTime2 = System.nanoTime();
+        Thread t2= new PerfectThread(533550336);
+        Thread t3= new PerfectThread(633550337);
+        Thread t4= new PerfectThread(935503371);
+        Thread t5= new PerfectThread(735503371);
+        long startTime2 = System.currentTimeMillis();
         t1.start();
         t2.start();
         t3.start();
-        long endTime2 = System.nanoTime();
+        t4.start();
+        t5.start();
+        while(t1.isAlive() || t2.isAlive() || t3.isAlive() || t4.isAlive() || t5.isAlive()){
+                // waits
+        }
+        long endTime2 = System.currentTimeMillis();
         long duration2 = endTime2 - startTime2;
         System.out.println("Duration Thread :" + duration2);
     }
