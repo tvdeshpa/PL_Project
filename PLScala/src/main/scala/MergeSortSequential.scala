8071758163bd0766865e61b1d74213d262f05a46
Akhil Nayabu <akhildn@gmail.com>
2017-11-29 07:34:53 -0800

    Update

--- PLScala/src/main/scala/MergeSortSequential.scala
+++ PLScala/src/main/scala/MergeSortSequential.scala
@@ -30,9 +30,9 @@ object MergeSortSequential extends App {
 
   def merge(left: Vector[Int], right: Vector[Int]): Vector[Int] = {
 
-    if(right.size == 1)
+    if(right.size == 0 && left.size > 0)
       left
-    else if(left.size == 1)
+    else if(left.size == 0 && right.size > 0)
       right
     else {
       if(left.head < right.head)
